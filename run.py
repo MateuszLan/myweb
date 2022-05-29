aef7c942f68370f0c51db3814d9f294c55ad0540
MateuszLan <92538483+MateuszLan@users.noreply.github.com>
2022-05-29 21:05:34 +0200

    git do chmury

--- run.py
+++ run.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 app.secret_key = secrets.token_hex(16) #generujemy sekretny klucz aplikacji
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '0' #zezwalamy na polaczenie w lokalnym srodowisku bez https
 github_blueprint = make_github_blueprint(
-client_id="7703588b0ebee5a9e41e", #tu wklek swoj wygenerowany id z github
-client_secret="89f998afca3c2770756b027585c6c5b4921c6de3",#tu wklej swoj wygenerowany client secret z github
+#client_id="7703588b0ebee5a9e41e", #lokalny
+#client_secret="89f998afca3c2770756b027585c6c5b4921c6de3",#tu wklej swoj wygenerowany client secret z github
+client_id="9bb0e5f0bfba6aea4e5e", #tu wklek swoj wygenerowany id z github (do chmury)
+client_secret="5f7d3186445e6a31b8ce385576e340b70eed1df0",#tu wklej swoj wygenerowany client secret z github(do chmury)
 )
 app.register_blueprint(github_blueprint, url_prefix='/login')
 
