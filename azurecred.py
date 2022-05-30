f122cfe9f09e215d6efd4741643b850c3c1f781f
MateuszLan <92538483+MateuszLan@users.noreply.github.com>
2022-05-30 17:59:42 +0200

    i tak nie zadziała??

--- azurecred.py
+++ azurecred.py
@@ -2,4 +2,4 @@ AZDBSERVER = 'baza76781.database.windows.net' #twoja nazwa utworzonego serwera
 AZDBNAME = 'Baza76781' #twoja nazwa bazy na serwerze
 AZDBUSER = 'Admin76781' #twoja nazwa użytkownika
 AZDBPW = 'Admin7678' #twoje hasło,którego nie powinniśmy znać
-AZDBDRIVER = '{ODBC Driver 17 for SQL Server}' #sterownik bazy danych, nic nie zmieniaj
+AZDBDRIVER = '{ODBC Driver 18 for SQL Server}' #sterownik bazy danych, nic nie zmieniaj
